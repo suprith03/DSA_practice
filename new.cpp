bae8852341dbae4865a861e8e55a6a69aa18793b
Suprith Singh <123349613+suprith03@users.noreply.github.com>
2025-03-29 22:36:38 +0530

    new

--- new.cpp
+++ new.cpp
@@ -157,7 +157,7 @@ int missingNumber(vector<int>& arr) {
 
 // using XOR O(n);
 // xor of same number is always 0
-// eg 3 ^ 3  = 0 etc etc.
+// eg 3 ^ 3  = 0.
 
 int XorMissingNumber(vector<int>& arr) {
     int n = arr.size();
@@ -169,6 +169,147 @@ int XorMissingNumber(vector<int>& arr) {
     return Xor^n;
 }
 
+int SLargestElement(vector<int>& arr) {
+    int n = arr.size();
+ 
+    sort(arr.begin(), arr.end());
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] != arr[n - 1]) {
+            return arr[i];
+        }
+    }
+    return -1;
+}
+
+int optiSLargest(vector<int>& arr) {
+    int n = arr.size();
+    int largestt = -1, sLargestt = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largestt) {
+            sLargestt = largestt;
+            largestt = arr[i];
+        } else if (arr[i] < largestt && arr[i] > sLargestt) {
+            sLargestt = arr[i];
+        }
+    }
+    return sLargestt;
+}
+
+int kthLargestElement(vector<int>& arr, int k) {
+    int n = arr.size();
+
+    priority_queue<int> pq;
+    // push ele
+    for (int i = 0; i < n; i++) {
+        pq.push(arr[i]);
+    }
+    //pop until reaches k
+    for (int i = 1; i < k; i++) {
+        pq.pop();
+    }
+    // return top ele
+    return pq.top();
+}
+
+int findDuplicates(vector<int>& arr) {
+    int slow = arr[0];
+    int fast = arr[0];
+
+    do {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+
+    slow = arr[0];
+    while (slow != fast) {
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+    return slow;
+}
+int dp_climbStairs(int n) {
+    if (n <= 1) 
+    return 1;
+
+    vector<int> dp(n);
+
+    dp[0] = 1;
+    dp[1] = 2;
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n - 1];
+}
+// best
+int climbStairs(int n) {
+    if (n <= 1)
+        return 1;
+
+    int prev1 = 1, prev2 = 2;
+    for (int i = 3; i <= n; i++) {
+        int curr = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curr;
+    }
+    return prev2;
+}
+// house robber using dp;
+int houseRobber(vector<int>& arr) {
+    int n = arr.size();
+
+    if (n == 0) 
+        return 0;
+    if (n == 1)
+        return arr[0];
+
+    vector<int> dp(n, 0);
+
+    dp[0] = arr[0];
+    dp[1] = max(arr[0], arr[1]);
+
+    for (int i = 2; i < n; i++) {
+        dp[i] = max(dp[i - 2] + arr[i], dp[i - 1]);
+    }
+    return dp[n - 1];
+}
+
+int noDPHouseRobber(vector<int>& arr) {
+    int n = arr.size();
+
+    if (n == 0)
+        return 0;
+    if ( n == 1)
+        return arr[0];
+
+    int prev1 = arr[0];
+    int prev2 = max(arr[0], arr[1]);
+
+    for (int i = 2; i < n; i++) {
+        int curr = max(prev1 + arr[i], prev2);
+        prev1 = prev2;
+        prev2 = curr;
+    }
+    return prev2;
+}
+
+int maxSubArray(vector<int>& arr) {
+    int n = arr.size();
+    int currSum = 0;
+    int maxSum = arr[0];
+
+    for (int i = 0; i < n; i++) {
+        currSum += arr[i];
+        maxSum = max(maxSum, currSum);
+
+        if (currSum < 0) {
+            currSum = 0;
+        }
+    }
+    return maxSum;
+}
 
 int main() {
     int n;
@@ -187,10 +328,30 @@ int main() {
     // cin >> x >> n;
     // cout << powerXN(x,n);
     
+    // vector<int> arr(n);
+    // for (int i = 0; i < n; i++) {
+    //     cin >> arr[i];
+    // }
+    // cin >> k;
+    // cout << kthLargestElement(arr, k);
+
+    // vector<int> arr(n);
+
+    // for (int i = 0; i < n; i++) {
+    //     cin >> arr[i];
+    // }
+    // cout << findDuplicates(arr);
+
+
+    // int ways = dp_climbStairs(n);
+    // cout << ways;
+
     vector<int> arr(n);
+ 
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    cout << XorMissingNumber(arr);
+    cout << maxSubArray(arr);
+
     return 0;
 }
